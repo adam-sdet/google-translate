186a128e79919e88a862b68a342bc0f2175ab6ec
Adam Gul Sdet <adamgul.sdet@gmail.com>
2022-10-19 08:56:02 -0500

    Reading source and translated texts and languages from .xlsx file

--- src/test/java/google/translate/tests/GoogleTranslateTest.java
+++ src/test/java/google/translate/tests/GoogleTranslateTest.java
@@ -3,7 +3,11 @@ package google.translate.tests;
 import google.translate.pages.TranslatePage;
 import google.translate.utilities.ConfigurationReader;
 import google.translate.utilities.Driver;
+import google.translate.utilities.ExcelReader;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.openqa.selenium.*;
 import org.openqa.selenium.support.ui.ExpectedCondition;
 import org.openqa.selenium.support.ui.ExpectedConditions;
@@ -13,34 +17,46 @@ import java.time.Duration;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
 public class GoogleTranslateTest {
 
   TranslatePage translatePage = new TranslatePage();
     private WebDriver driver = Driver.getDriver();
     WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+    ExcelReader excelReader =new ExcelReader();
     JavascriptExecutor jse = (JavascriptExecutor) driver;
-    String sourceLanguage = "&sl=" + "az";
-    String targetLanguage = "&tl=" + "en";
+    String sourceFilePath = "src/test/resources/xlsx/SourceText.xlsx";
+    String translatedFilePath = "src/test/resources/xlsx/TranslatedText.xlsx";
+    String languagesFilePath = "src/test/resources/xlsx/Languages.xlsx";
 
-//    private WebDriver driver = Driver.getHeadlessChrome();
+
+    @BeforeAll
+    public void init() {
+        String sourceLanguageCode = excelReader.readCellData(1, 1, languagesFilePath);
+        String translatedLanguageCode = excelReader.readCellData(2, 1, languagesFilePath);
+        String sourceLanguage = "&sl=" + sourceLanguageCode;
+        String targetLanguage = "&tl=" + translatedLanguageCode;
+        driver.get(ConfigurationReader.getProperty("google-translate") + sourceLanguage + targetLanguage);
+    }
 
     @Test
     public void TranslateTest() throws InterruptedException {
-        driver.get(ConfigurationReader.getProperty("google-translate") + sourceLanguage + targetLanguage);
 
         String title = driver.getTitle();
         assertEquals("Google Translate", title);
 
-        translatePage.sourceText.sendKeys("Salam");
+        String sourceText = excelReader.readCellData(1, 1, sourceFilePath);
+        translatePage.sourceText.sendKeys(sourceText);
         System.out.println("translated text is: " + translatePage.translatedText.getText());
 
-        assertEquals("Hello", translatePage.translatedText.getText());
+        String translatedText = excelReader.readCellData(1, 1, translatedFilePath);
+        assertEquals(translatedText, translatePage.translatedText.getText());
 
         translatePage.swapLanguages.click();
 
-        wait.until(ExpectedConditions.textToBePresentInElement(translatePage.translatedText, "Salam"));
+        wait.until(ExpectedConditions.textToBePresentInElement(translatePage.translatedText, sourceText));
         System.out.println("swapped translated text is: " + translatePage.translatedText.getText());
-        assertEquals("Salam", translatePage.translatedText.getText());
+        assertEquals(sourceText, translatePage.translatedText.getText());
 
 
 
@@ -57,8 +73,11 @@ public class GoogleTranslateTest {
         jse.executeScript("arguments[0].click();", targetLanguage);
         */
 
-         Driver.closeDriver();
+    }
 
+    @AfterAll
+    public void teardown() {
+        Driver.closeDriver();
     }
 
 
---
+++ src/test/java/google/translate/utilities/ExcelReader.java
@@ -0,0 +1,39 @@
+package google.translate.utilities;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.*;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+
+public class ExcelReader {
+
+    public String readCellData(int vRow, int vColumn, String filePath)
+    {
+        String value=null;          //variable for storing the cell value
+        Workbook wb=null;           //initialize Workbook null
+        try
+        {
+
+            FileInputStream fis=new FileInputStream(filePath); //reading data from a file in the form of bytes
+            //constructs an XSSFWorkbook object, by buffering the whole stream into the memory
+            wb=new XSSFWorkbook(fis);
+        }
+        catch(FileNotFoundException e)
+        {
+            e.printStackTrace();
+        }
+        catch(IOException e1)
+        {
+            e1.printStackTrace();
+        }
+        Sheet sheet=wb.getSheetAt(0);    //getting the XSSFSheet object at given index
+        Row row=sheet.getRow(vRow);         //returns the logical row
+        Cell cell=row.getCell(vColumn);     //getting the cell representing the given column
+        value=cell.getStringCellValue();    //getting cell value
+        return value;                       //returns the cell value
+    }
+}
